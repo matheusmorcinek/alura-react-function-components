b1079cb9f52634ef5a3a904acad5d76d19e431b9
Matheus Morcinek <matheusmorcinek@gmail.com>
2021-02-18 16:14:08 -0300

    trabalhando com hooks

--- projeto-atual/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ projeto-atual/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from '@material-ui/core';
 import TextField from '@material-ui/core/TextField';
 import Switch from '@material-ui/core/Switch';
@@ -6,19 +6,65 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 
 function FormularioCadastro() {
 
-    return (
-        <form>
+    const [nome, setNome] = useState('');
+    //o hook useState devolve um array de dois elementos
+    //a primeira é a referência para o valor do estado atual e a segunda é a função que altera esse estado.
 
-            <TextField margin='normal' id="nome" label="Nome" variant="outlined" type='text' fullWidth />
-            <TextField margin='normal' id="sobrenome" label="Sobrenome" variant="outlined" type='text' fullWidth />
-            <TextField margin='normal' id="cpf" label="CPF" variant="outlined" type='text' fullWidth />
+    //guarda o estado interno do componente, e toda vez que esse state for alterado o React força a reenderização desse component
+    //vai manter este state durante todo o ciclo de vida
+    //antes usavamos o state com class componente, mas agora em function components usamos o hook useState
+    // state = {
+    // };
+
+    const [sobrenome, setSobrenome] = useState('');
+    // const arr = useState('Morcinek');
+    // const sobrenome = arr[0];
+    // const setSobrenome = arr[1];
+
+    const [cpf, setCPF] = useState('');
+    const [promocoes, setPromocoes] = useState(true);
+    const [novidades, setNovidades] = useState(true);
+
+    return (
+        <form onSubmit={(event) => {
+            
+            event.preventDefault();
+            console.log({nome, sobrenome, cpf, novidades, promocoes});
+        }}>
+
+            <TextField
+                value={nome}
+                onChange={(event) => {
+
+                    // let tempNome = event.target.value;
+                    // if (tempNome.length >= 3) {
+
+                    //     tempNome = tempNome.substring(0, 3);
+                    // }
+
+                    setNome(event.target.value);
+                }} margin='normal' id="nome" label="Nome" variant="outlined" type='text' fullWidth />
+
+            <TextField value={sobrenome}
+                onChange={(event) => {
+
+                    setSobrenome(event.target.value);
+                }} margin='normal' id="sobrenome" label="Sobrenome" variant="outlined" type='text' fullWidth />
+
+            <TextField value={cpf} onChange={(event) => { setCPF(event.target.value) }} margin='normal' id="cpf" label="CPF" variant="outlined" type='text' fullWidth />
 
             <FormControlLabel
                 control={
                     <Switch
-                        defaultChecked
+                        checked={promocoes}
                         name="Promoções"
                         inputProps={{ 'aria-label': 'primary checkbox' }}
+                        onChange={
+                            (event) => {
+
+                                setPromocoes(event.target.checked)
+                            }
+                        }
                     />
                 }
                 label="Promoções"
@@ -27,14 +73,15 @@ function FormularioCadastro() {
             <FormControlLabel
                 control={
                     <Switch
-                        defaultChecked
+                        checked={novidades}
                         name="Novidades"
                         inputProps={{ 'aria-label': 'primary checkbox' }}
+                        onChange={(event) => { setNovidades(event.target.checked) }}
                     />
                 }
                 label="Novidades"
             />
- 
+
             <Button color='primary' variant='contained' type='submit'>Cadastrar</Button>
         </form>
     );
